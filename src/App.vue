19bbf7deac0606281d8a7d78c09130b86c5b39e8
xiong1452 <2862044980@qq.com>
2021-08-03 17:15:28 +0800

    Icon component complete

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,29 @@
   <div id="app">
     <h2>VUE 组件</h2>
     <Avatar :url="StaticImage" :width="100" :height="100"></Avatar>
+    <Avatar :url="StaticImage2" :width="100" :height="100"></Avatar>
+    <Icon :type="'weixin'"></Icon>
+    <Icon :type="'error'"></Icon>
   </div>
 </template>
 
 <script>
 import Avatar from "./components/Avatar";
 import StaticImage from "./assets/images/IDR_PROFILE_AVATAR_43@2x.png";
+import StaticImage2 from "./assets/images/IDR_PROFILE_AVATAR_53@2x.png"
+import Icon from "./components/Icon.vue"
 
 export default {
   name: "App",
   data() {
     return {
       StaticImage,
+      StaticImage2
     };
   },
   components: {
     Avatar,
+    Icon
   },
 };
 </script>
@@ -30,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.iconfont{
+  font-size: 24px;
+  color: red;
 }
 </style>
---
+++ src/components/Icon.vue
@@ -0,0 +1,48 @@
+<template>
+  
+    <i class="iconfont" :class="changeClass"></i>
+
+</template>
+
+<script>
+const IconMap = {
+    home: 'icon-huaban-copy',
+    success: 'icon-querenyuanzhengqueduigoutijiaochenggongwancheng',
+    error: 'icon-cuowu',
+    close: 'icon-guanbi',
+    warn: 'icon-jinggao',
+    info: 'icon-cascadeviewattr',
+    blog: 'icon-blog',
+    code: 'icon-code',
+    about: 'icon-set_about_hov',
+    weixin: 'icon-weixin',
+    mail: 'icon-email',
+    github: 'icon-huaban88',
+    qq: 'icon-qq',
+    arrowUp: 'icon-icon',
+    arrowDown: 'icon-icon1',
+    empty: 'icon-empty',
+    chat: 'icon-liuyan'
+}
+
+export default {
+    name: "Icon",
+    props: {
+        type: {
+            type: String,
+            require: true
+        }
+    },
+    computed: {
+        changeClass(){
+            return IconMap[this.type];
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import url("//at.alicdn.com/t/font_2719464_6b3joba2yse.css");
+
+
+</style>
