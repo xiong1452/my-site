b700449636438e1bea42e9d9249e9dd04d840289
xiong1452 <2862044980@qq.com>
2021-08-04 15:44:57 +0800

    Empty component complete

---
+++ src/components/Empty/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="empty-container">
+    <div class="empty-item">
+      <Icon :type="'empty'"></Icon>
+    </div>
+    <div class="empty-text">
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from "../Icon";
+export default {
+  name: "Empty",
+  props: {
+    text: {
+      type: String,
+      default: "无数据",
+    },
+  },
+  components: {
+    Icon,
+  },
+};
+</script>
+
+<style lang='less' scoped>
+@import url("../../styles/var.less");
+.empty-container {
+  height: 100%;
+  .flex-center();
+  align-content: center;
+  flex-wrap: wrap;
+  .empty-item {
+    width: 100%;
+    text-align: center;
+    i {
+      font-size: 100px;
+      color: @gray;
+    }
+  }
+  .empty-text {
+    margin-top: 30px;
+    color: @gray;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ src/components/Empty/test.vue
@@ -0,0 +1,36 @@
+<template>
+  
+    <div class="app">
+        <h1>Vue 组件</h1>
+        <Empty  :text="text"></Empty>
+    </div>
+
+</template>
+
+<script>
+import Empty from '../Empty'
+export default {
+    data() {
+        return {
+            text: '无显示'
+        }
+    },
+    components: {
+        Empty
+    }
+}
+</script>
+
+<style>
+@import url("../../styles/reset.less");
+body, html{
+    width: 100%;
+    height: 100%;
+}
+
+.app{
+    width: 800px;
+    height: 800px;
+}
+
+</style>
