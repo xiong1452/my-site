abff403c4c45aa553fd82baaad33e0a8a239dd76
xiong1452 <2862044980@qq.com>
2021-08-13 17:22:56 +0800

    about component complete

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,13 +1,34 @@
 <template>
-    <h1>关于</h1>
+    <div class='demo'>
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/lAIGb1lfpBw" title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+        </iframe>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "about",
+
     }
 </script>
 
 <style scoped>
+    .demo {
+        display: block;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        line-height: 100px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        overflow: hidden;
+        background: #fff;
+        position: relative;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, .2);
+        margin-right: 4px;
+    }
 
 </style>
