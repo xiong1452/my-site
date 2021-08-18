4784dd9ce04284a4c74240a9de73006011256b4b
xiong1452 <2862044980@qq.com>
2021-08-18 15:03:35 +0800

    nprogress complete

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,42 +1,113 @@
+import "nprogress/nprogress.css"
+import {start, done, configure} from "nprogress"
+
+configure({
+    trickleSpeed: 20
+})
+
+function delay(duration) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, duration)
+    })
+}
+
 export default [
     {
-        name: "home", path: "/", component: () => import("@/views/Home"),
+        name: "home", path: "/", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/Home");
+            done();
+            return comp;
+        },
         meta: {
             title: "首页"
         }
     },
     {
-        name: 'about', path: "/about", component: () => import("@/views/About"),
+        name: 'about', path: "/about", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/About");
+            done();
+            return comp;
+        },
         meta: {
             title: "关于"
         }
     },
     {
-        name: 'blog', path: "/blog", component: () => import("@/views/Blog"),
+        name: 'blog', path: "/blog", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/Blog");
+            done();
+            return comp;
+        },
         meta: {
             title: "文章"
         }
     },
     {
-        name: 'category', path: "/blog/cate/:categoryId", component: () => import("@/views/Blog"),
+        name: 'category', path: "/blog/cate/:categoryId", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/Blog")
+            done();
+            return comp;
+        },
         meta: {
             title: "文章列表"
         }
     },
     {
-        name: 'detail', path: "/blog/:id", component: () => import("@/views/Blog/Detail"),
+        name: 'detail', path: "/blog/:id", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/Blog/Detail");
+            done();
+            return comp;
+        },
         meta: {
             title: "文章详情"
         }
     },
     {
-        name: 'code', path: "/Code", component: () => import("@/views/Code"),
+        name: 'code', path: "/Code", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/Code");
+            done();
+            return comp;
+        },
         meta: {
             title: "项目&描述"
         }
     },
     {
-        name: 'chat', path: "/chat", component: () => import("@/views/Chat"),
+        name: 'chat', path: "/chat", component: async () => {
+            start()
+            if (process.env.NODE_ENV === "development") {
+                await delay(2000)
+            }
+            const comp = await import("@/views/Chat");
+            done();
+            return comp;
+        },
         meta: {
             title: "留言板"
         }
